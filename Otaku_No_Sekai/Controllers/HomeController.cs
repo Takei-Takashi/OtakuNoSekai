95e125e128d4b97fdcf93d14e5cd86aad49345be
longvdhhe141608 <watbdragonhoang1201@gmail.com>
2023-05-22 16:27:57 +0700

    Add project files.

---
+++ Otaku_No_Sekai/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Otaku_No_Sekai.Models;
+using System.Diagnostics;
+
+namespace Otaku_No_Sekai.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Otaku_No_Sekai/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Otaku_No_Sekai.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
